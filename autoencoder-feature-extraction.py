d449b7e66a57909fa51dff6e0fec5f64c4d72f8b
Abdullah Almamun <165108040+almamun-de@users.noreply.github.com>
2025-03-13 23:28:24 +0100

    Update autoencoder-feature-extraction.py

--- autoencoder-feature-extraction.py
+++ autoencoder-feature-extraction.py
@@ -1,4 +1,13 @@
 
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+import keras
+import tensorflow as tf
+import argparse
+import time
+from memory_profiler import memory_usage
+
 # Function to extract features from a dataset using an autoencoder.
 # It applies different scenarios for data filtering and uses k-fold cross-validation.
 def extract_feature(autoencoder, encoder, dataset_df, scenario, k):
